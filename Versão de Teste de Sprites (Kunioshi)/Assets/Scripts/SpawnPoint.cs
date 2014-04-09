9a652b7a70af4af529a3f8ed5a76841dec35695f
Kunioshi <filipekunioshi@hotmail.com>
2014-04-09 01:28:40 -0300

    Adicionando alguns Raw e uma versão de Unity test
    A versão de Unity é uma versão que eu estou usando para testar as
    animações e coisas parecidade.
    Mas no final tem bastante coisa que TALVEZ seja usada na versão final

---
+++ Versão de Teste de Sprites (Kunioshi)/Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPoint : MonoBehaviour {
+	public Material startMat;
+	public Material ativatedMat;
+	public GameObject checkPointCube;
+
+	// Use this for initialization
+	void Start () {
+		// Change self colors
+		particleSystem.startColor = startMat.color;
+		
+		checkPointCube.renderer.material = startMat;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0, 2, 0);
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.collider.tag.Equals("Player")) {
+			// Change player's check point
+			Character playerScript = other.GetComponent<Character>();
+			playerScript.checkPoint = transform;
+
+			// Change self colors
+			particleSystem.startColor = ativatedMat.color;
+
+			checkPointCube.renderer.material = ativatedMat;
+		}
+	}
+}
