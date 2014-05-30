5276982a298a6e6b5650d3ccd722315442797f29
Victor Cantelli <v.cantelli@hotmail.com>
2014-05-30 14:42:41 -0300

    Menus

---
+++ Jogo/Assets/Scripts/Menu/clique.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class clique : MonoBehaviour {
+
+    int selecao = 0;
+    public int pos;
+    bool intro;
+	bool ControlVertical = true;
+
+	// Use this for initialization
+	void Start () {
+        intro = true;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+               
+		if(Input.GetAxis("Vertical") == 0)
+	    {
+			ControlVertical = true;
+		}
+
+        if (!intro)
+        {
+			if ((Input.GetKeyDown("down") || Input.GetAxis("Vertical") < 0) && selecao < 3 && ControlVertical)
+            {
+                selecao++;
+				ControlVertical = false;
+            }
+
+			if ((Input.GetKeyDown("up") || Input.GetAxis("Vertical") > 0)  && selecao > 0 && ControlVertical)
+            {
+                selecao--;
+				ControlVertical = false;
+            }
+
+            if (selecao == pos)
+            {
+                renderer.material.color = Color.yellow;
+            }
+            else
+            {
+                renderer.material.color = Color.white;
+            }
+
+			if (Input.GetKeyDown("return") || Input.GetButton("Fire1"))
+            {
+                switch (selecao)
+                {
+                    case 0:
+                        Application.LoadLevel("CriarSala");
+                        break;
+                    case 1:
+                        Application.LoadLevel("Instrucoes");
+                        break;
+                    case 2:
+                        Application.LoadLevel("Creditos");
+                        break;
+                    case 3:
+                        Application.Quit();
+                        break;
+                }
+
+            }
+        }
+        else
+        {
+			if (Input.GetKeyDown("return") || Input.GetButton("joystick button 7"))
+            {
+                intro = false;
+            }
+        }
+	
+	}
+}
---
+++ Jogo/Assets/Scripts/Menu/menucriarsala.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menucriarsala : MonoBehaviour {
+
+    int selecao = 0;
+    private int modo = 0; // 0 = inicio / 1 = criar sala / 2 = entrar em sala
+    public GameObject criarsala;
+    public GameObject entrarsala;
+    public GameObject posicao_popup;
+    public GameObject posicao_inativo;
+    public TextMesh entrar;
+    public TextMesh criar;
+	bool ControlVertical = true;
+
+	// Use this for initialization
+	void Start () {
+        criarsala.transform.position = posicao_inativo.transform.position;
+        entrarsala.transform.position = posicao_inativo.transform.position;
+        criar.color = Color.yellow;
+        entrar.color = Color.white;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+		if(Input.GetAxis("Vertical") == 0)
+		{
+			ControlVertical = true;
+		}
+
+		if ((Input.GetKeyDown("down") || Input.GetAxis("Vertical") < 0) && ControlVertical)
+        {
+            selecao = 1;
+            criar.color = Color.white;
+            entrar.color = Color.yellow;
+			ControlVertical = false;
+        }
+
+		if ((Input.GetKeyDown("up") || Input.GetAxis("Vertical") > 0) && ControlVertical)
+        {
+            selecao = 0;
+            criar.color = Color.yellow;
+			entrar.color = Color.white;
+			ControlVertical = false;
+        }
+
+		if (Input.GetKeyDown("escape") || Input.GetButton("Fire2"))
+        {
+            if(modo == 0)
+                Application.LoadLevel("Main Menu");
+            else if(modo == 1)
+                Application.LoadLevel(Application.loadedLevel);
+            else if(modo == 2)
+                Application.LoadLevel(Application.loadedLevel);
+        }
+
+		if (Input.GetKeyDown("return") || Input.GetButton("Fire1"))
+        {
+            if (modo == 0)
+            {
+                switch (selecao)
+                {
+                    case 0:                        
+                        criarsala.transform.position = posicao_popup.transform.position;
+                        entrarsala.transform.position = posicao_inativo.transform.position;
+                        entrar.transform.position = posicao_inativo.transform.position;
+                        criar.transform.position = posicao_inativo.transform.position;
+                        modo = 1;
+                        break;
+                    case 1:                        
+                        entrarsala.transform.position = posicao_popup.transform.position;
+                        criarsala.transform.position = posicao_inativo.transform.position;
+                        entrar.transform.position = posicao_inativo.transform.position;
+                        criar.transform.position = posicao_inativo.transform.position;
+                        modo = 2;
+                        break;
+                }
+            }
+            else if (modo == 1)
+                Debug.Log("Criar Sala");
+            else if (modo == 2)
+                Debug.Log("Entrar em sala");           
+
+        }
+	}
+}
